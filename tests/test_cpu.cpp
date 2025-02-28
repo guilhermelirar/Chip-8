c9cfdcedeb2d8542e0b09412a6c96dd68e9ea0d1
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-02-28 10:36:47 -0300

    test: 00E0 (CLS) clears screen

--- tests/test_cpu.cpp
+++ tests/test_cpu.cpp
@@ -3,7 +3,18 @@
 #include "chip8.hpp"
 #include "cpu.hpp"
 
+static CHIP8 c;
+
 TEST_CASE("Program counter starts at 0x200", "[CPU]") {
-  CHIP8 c;
   REQUIRE(c.cpu.pc == 0x200);
+}
+
+TEST_CASE("Opcode 0x00E0 (CLS) clears display)", "[CPU and Screen]") {
+  memset(c.screen.buffer, true, sizeof(c.screen.buffer)); // Forcing all pixels to true
+  c.cpu.DecodeAndExecute(0x00E0);                         // Calls instruction
+  
+  // Checks if instruction worked
+  for (int i = 0; i < 64 * 32; i++) { 
+    REQUIRE_FALSE(c.screen.buffer[i]); 
+  }
 }
