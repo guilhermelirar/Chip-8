5e5cc1aeebbf456fe02e9a12e4bcca5ece584683
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-01 21:30:31 -0300

    feat: instruction (3/35) Load Vx, byte

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -6,17 +6,28 @@ CPU::CPU(CHIP8* chip8): chip8(chip8) {
 }
 
 void CPU::DecodeAndExecute(uint16_t opcode) {
-  // Fixed opcodes
-  switch (opcode) {
-    case (0x00E0): {
-      return chip8->screen.Clear(); 
-    }
-  }
-  
   // Variable opcodes
   switch (opcode >> 0xC) {
+    // Fixed opcodes
+    case (0): {
+      // 0x00E0 CLS Clear Screen
+      if (opcode == 0x00E0)
+        chip8->screen.Clear();
+      break;
+    }
+    
+    // 0x1NNN JMP to addr
     case (0x1): {
-      pc = 0xEFF & opcode;
+      pc = opcode & 0x0FFF;
+      break;
+    }
+
+    // 0x6XNN LOAD X to NN
+    case (0x6): {
+      uint8_t reg = (0x0F00 & opcode) >> 8;
+      uint8_t value = (0x00FF & opcode); 
+      V[reg] = value;
+      break;
     }
   }  
 }
