b165861088947ba05513831d4c90fa1fa93f3666
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-03 21:53:06 -0300

    Starting Screen::drawSprite

--- include/screen.hpp
+++ include/screen.hpp
@@ -1,16 +1,24 @@
 #ifndef SCREEN_HPP
 #define SCREEN_HPP
 
+#include <cstdint>
+
 class CHIP8;
 
 class Screen {
 public:
-  CHIP8* chip8;         // CHIP-8 System
+  CHIP8 *chip8;         // CHIP-8 System
   bool buffer[0x800];   // Pixel buffer
- 
-  Screen(CHIP8* chip8); // Constructor
-  
+
+  Screen(CHIP8 *chip8); // Constructor
+
   void Clear();         // Clears display
+  
+  // Draws a sprite of certain height,
+  // at coordinates x and y
+  void drawSprite(uint8_t x, uint8_t y, 
+                  uint8_t spriteHeight, 
+                  uint8_t *sprite);
 };
 
 #endif // SCREEN_HPP
--- src/screen.cpp
+++ src/screen.cpp
@@ -8,3 +8,8 @@ Screen::Screen(CHIP8* chip8): chip8(chip8) {}
 void Screen::Clear() {
   memset(buffer, false, 64 * 32);
 }
+
+void Screen::drawSprite(uint8_t x, uint8_t y, 
+                  uint8_t spriteHeight, 
+                  uint8_t *sprite) {}
+
