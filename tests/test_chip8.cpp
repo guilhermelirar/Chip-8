f60ddc418ff8369914e40e009885fc811f6175d3
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-03 21:53:26 -0300

    test: Screen::DrawSprite and behavior on collision

--- tests/test_chip8.cpp
+++ tests/test_chip8.cpp
@@ -7,3 +7,19 @@ TEST_CASE("Fonts are initialized between 050-09F", "[CHIP-8]") {
     REQUIRE(c.memory[i] != 0);
   }
 }
+
+TEST_CASE("Sprite drawing results in correct behavior of VF", "[SCREEN]") {
+  CHIP8 c;
+  c.cpu.DecodeAndExecute(0x6F00); // Sets VF to 0
+  
+  // 4x4 square
+  uint8_t sprite[] = {0xF0, 0x90, 0x90, 0xF0};
+
+  // Drawing sprite at 0, 0
+  c.screen.drawSprite(0, 0, 4, sprite);
+  REQUIRE(c.cpu.V[0xF] == 0); // No collision
+  
+  // Drawing sprite at same position to force collision 
+  c.screen.drawSprite(0, 0, 4, sprite);
+  REQUIRE(c.cpu.V[0xF] == 1);
+}
