3a3d33a962c14c58b1e37f23fc380d8ec05f3f14
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2025-03-18 19:47:00 -0300

    chip8: frameStart to deal with time

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -10,6 +10,9 @@ public:
   static constexpr uint8_t FONT_DATA_START = 0x50;
   static constexpr uint8_t FONT_SPRITE_HEIGHT = 5;
   static constexpr uint16_t MEMORY_SIZE = 0x1000;
+
+  uint32_t frameStart;
+
   uint8_t memory[MEMORY_SIZE];  // 4kb memory
   Interpreter interpreter;      // System Interpreter
   Screen screen;
